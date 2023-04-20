297509490c00d6cc0cb80496ba133f8b366ef915
elisheba-beep <ekunife35@gmail.com>
2023-04-20 08:13:33 +0100

    binary search trees

---
+++ trees/trees.py
@@ -0,0 +1,113 @@
+# a tree is a nonlinear heirachical data structure that consists of nodes connected by edges. Each Tree consists of a root node from which we can access each element of the tree.
+# linked lists are linear
+# terminologies
+# 1. node(the last nodes are called leaf nodes)
+# 2. edge(link btw two nodes)
+# 3. root(topmost node of a tree)
+# 4. height
+# 5. depth
+# 6. degree
+# binary tree - each parent node can have at most two children. Each node of a binary tree cnsists of trhee items:
+# data item, address of left child, address of right child
+# types: full, perfect and complete binary tree
+# full:every parent node has either two or no children
+# perfect:every internal node has exactly two child nodes
+# complete: like a full binary tree but:every level must be completely filled, all leaf elements must link towards the left and the last leaf element migh not have a right sibling
+
+# Binary Search Tree(slides) - 42, 100, 50,51...
+# everything less than 42 goes to the left, but greater than goes to the right
+
+
+# the node constructor
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+# binary search tree
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        newNode = Node(value)
+        if self.root is None:
+            self.root = newNode
+            return self
+        temp = self.root
+        while True:
+            if newNode.value == temp.value:
+                return
+            if newNode.value < temp.value:
+                if temp.left is None:
+                    temp.left = newNode
+                    return self
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = newNode
+                    return self
+                temp = temp.right
+
+    def printTree(self):
+        leftValue = self.root.left
+        rightValue = self.root.right
+        lefts = 'left values: '
+        rights = 'right values: '
+
+        while leftValue:
+            lefts += str(leftValue.value) + '->'
+            leftValue = leftValue.left
+
+        while rightValue:
+            rights += str(rightValue.value) + '->'
+            rightValue = rightValue.right
+        print('root: ' + str(self.root.value) + ' ' + lefts + ' ' + rights)
+
+    def contains(self, value):
+        if self.root is None:
+            return False
+
+        temp = self.root
+        while temp:
+            if value > temp.value:
+                temp = temp.right
+            elif value < temp.value:
+                temp = temp.left
+            else:
+                return True
+        return False
+    
+    def minValueNode(self, currentNode):
+        if self.root is None:
+            return
+        while currentNode.left:
+            currentNode = currentNode.left
+        return currentNode
+    
+    
+    def maxValueNode(self, currentNode):
+        if self.root is None:
+            return
+        while currentNode.right:
+            currentNode = currentNode.right
+        return currentNode
+        
+
+
+tree = BST()
+tree.insert(8)
+tree.insert(18)
+tree.insert(6)
+tree.insert(60)
+tree.insert(4)
+
+
+tree.printTree()
+print(tree.contains(8))
+print(tree.minValueNode(tree.root).value)
+print(tree.maxValueNode(tree.root).value)
+
